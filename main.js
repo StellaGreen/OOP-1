d5eada175f587467544fba4d94e2117380c925cb
StellaGreen <vickysoler5@gmail.com>
2021-04-01 17:51:55 +0200

    add important comment

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const { Point } = require('./point')
 
 //---------HUMAN ---------------
 const alice = new human('Alice','Liddell', 27, ['Html', 'Css', 'Php', 'C++'])
-alice.printInfo()
+alice.printInfo() // warning if you want run this code, get in classInfo the last command to executing in comment or all module are read
 
 // --------- POINT ------------
 const p1 = new Point(10, 12)
